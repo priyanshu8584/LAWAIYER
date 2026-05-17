d3654350b63528e9ee2225f08b60040a9f798296
priyanshu8584 <singh.priyanshu.1025@gmail.com>
2026-05-17 11:49:30 +0530

    fixed unexported libs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,22 +1,12 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is missing.");
-}
-
-const adapter = new PrismaPg({ connectionString });
-
 export const prisma =
   global.prisma ??
   new PrismaClient({
-    adapter,
     log: ["error"],
   });
 
